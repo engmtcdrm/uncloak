8cfae62ef4f93dd7478b506f66c525ccf9886157
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-08-14 17:54:20 -0400

    feat: initial code to add flag for file output

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"os"
 
 	pp "github.com/engmtcdrm/go-prettyprint"
 	"github.com/spf13/cobra"
@@ -19,6 +20,7 @@ const (
 	coverageThresholdUsage = "(optional) coverage threshold override. This will also overwrite what is specified in the configuration file"
 	debugUsage             = "(optional) enable debug output, e.g. what commands are run"
 	gitTargetRefUsage      = "(optional) git target ref to compare against (default: current branch's nearest parent branch)"
+	outputUsage            = "(optional) output file to write missing coverage to."
 	verboseUsage           = "(optional) enable verbose output, e.g. output from go test command"
 )
 
@@ -31,6 +33,7 @@ type cmd struct {
 	debug             bool
 	gitTargetRef      string
 	verbose           bool
+	output            string
 }
 
 func init() {
@@ -50,8 +53,8 @@ func init() {
 	rootCmd.Flags().Float64VarP(&c.coverageThreshold, "coverage-threshold", "c", config.DefaultConfig.CoverageThreshold, coverageThresholdUsage)
 	rootCmd.Flags().BoolVarP(&c.debug, "debug", "d", false, debugUsage)
 	rootCmd.Flags().StringVarP(&c.gitTargetRef, "target-ref", "t", "", gitTargetRefUsage)
+	rootCmd.Flags().StringVarP(&c.output, "output", "o", "", outputUsage)
 	rootCmd.Flags().BoolVarP(&c.verbose, "verbose", "v", false, verboseUsage)
-
 }
 
 // Execute executes the root command.
@@ -83,22 +86,8 @@ func (c *cmd) run(cmd *cobra.Command, args []string) error {
 		fmt.Println(string(report.CoverageProfile.RawTestOutput))
 	}
 
-	if report.HasUncoveredLines() {
-		fmt.Printf("%s\n\n", colors.LightGreen("Missing coverage:"))
-
-		for _, file := range report.Files {
-			for _, lineRange := range file.UncoveredNewLineGroups {
-				fmt.Printf("%s:%s:%s\n",
-					pp.Bold(file.Path),
-					pp.Redf("%d", lineRange.Start),
-					pp.Redf("%d", lineRange.End),
-				)
-			}
-
-			if len(file.UncoveredNewLineGroups) > 0 {
-				fmt.Println()
-			}
-		}
+	if err := outputUncoveredLines(report, c.output); err != nil {
+		return err
 	}
 
 	if err != nil {
@@ -113,9 +102,12 @@ func (c *cmd) run(cmd *cobra.Command, args []string) error {
 		pp.Greenf(floatFormat, report.CoveragePercent()),
 		pp.Greenf(floatFormat, cfg.CoverageThreshold),
 	)
+
 	return nil
 }
 
+// handleFlags updates the configuration based on the command-line flags that
+// were set.
 func (c *cmd) handleFlags(cfg *config.Config, cmd *cobra.Command) {
 	if cmd.Flags().Changed("coverage-threshold") {
 		cfg.CoverageThreshold = c.coverageThreshold
@@ -128,4 +120,47 @@ func (c *cmd) handleFlags(cfg *config.Config, cmd *cobra.Command) {
 	if cmd.Flags().Changed("target-ref") {
 		cfg.GitDiffOptions.TargetRef = c.gitTargetRef
 	}
+}
+
+// outputUncoveredLines writes the uncovered lines from the report to
+// [os.Stdout]. If an outputFile is specified, the uncovered lines will also be
+// written to that file.
+func outputUncoveredLines(report *analyzer.Report, outputFile string) error {
+	if !report.HasUncoveredLines() {
+		return nil
+	}
+
+	var ofile *os.File
+	if outputFile != "" {
+		var err error
+		ofile, err = os.Create(outputFile)
+		if err != nil {
+			return fmt.Errorf("failed to create output file: %w", err)
+		}
+		defer ofile.Close()
+	}
+
+	fmt.Printf("%s\n\n", colors.LightGreen("Missing coverage:"))
+
+	const fileFormat = "%s:%d:%d\n"
+
+	for _, file := range report.Files {
+		for _, lineRange := range file.UncoveredNewLineGroups {
+			fmt.Printf("%s:%s:%s\n",
+				pp.Bold(file.Path),
+				pp.Redf("%d", lineRange.Start),
+				pp.Redf("%d", lineRange.End),
+			)
+
+			if ofile != nil {
+				fmt.Fprintf(ofile, fileFormat, file.Path, lineRange.Start, lineRange.End)
+			}
+		}
+
+		if len(file.UncoveredNewLineGroups) > 0 {
+			fmt.Println()
+		}
+	}
+
+	return nil
 }
