09371a83fcd85d569d0867375d4ae112e6ab6e69
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-08-25 19:43:40 -0400

    test: added mor tests for taskCanceledError

--- internal/analyzer/errors_test.go
+++ internal/analyzer/errors_test.go
@@ -16,6 +16,12 @@ func Test_newTaskCanceledError(t *testing.T) {
 		assert.Error(t, err.e)
 		assert.Equal(t, underlyingErr, err.e)
 	})
+
+	t.Run("should create a new taskCanceledError with nil if the given error is nil", func(t *testing.T) {
+		err := newTaskCanceledError(nil)
+		assert.Error(t, err)
+		assert.Nil(t, err.e)
+	})
 }
 
 // Tests for [taskCanceledError.Error] function.
@@ -28,6 +34,12 @@ func Test_taskCanceledError_Error(t *testing.T) {
 		assert.Equal(t, taskErr.e, err)
 		assert.Equal(t, expectedMessage, taskErr.Error())
 	})
+
+	t.Run("should return an empty string if the underlying error is nil", func(t *testing.T) {
+		taskErr := newTaskCanceledError(nil)
+		assert.Error(t, taskErr)
+		assert.Nil(t, taskErr.e)
+	})
 }
 
 // Tests for [taskCanceledError.Unwrap] function.
@@ -37,5 +49,11 @@ func Test_taskCanceledError_Unwrap(t *testing.T) {
 		taskErr := newTaskCanceledError(underlyingErr)
 		assert.Error(t, taskErr)
 		assert.Equal(t, underlyingErr, taskErr.Unwrap())
+	})
+
+	t.Run("should return nil if the underlying error is nil", func(t *testing.T) {
+		taskErr := newTaskCanceledError(nil)
+		assert.Error(t, taskErr)
+		assert.Nil(t, taskErr.Unwrap())
 	})
 }
