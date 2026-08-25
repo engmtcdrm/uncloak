e1f8cdd47a7ebdae11925602e9fcc2a0d8d95371
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-08-25 19:45:34 -0400

    fix: fixed linting issues

--- internal/analyzer/errors_test.go
+++ internal/analyzer/errors_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 // Tests for [newTaskCanceledError] function.
@@ -12,15 +13,15 @@ func Test_newTaskCanceledError(t *testing.T) {
 	t.Run("should create a new taskCanceledError with the given error", func(t *testing.T) {
 		underlyingErr := errors.New("original error")
 		err := newTaskCanceledError(underlyingErr)
-		assert.Error(t, err)
-		assert.Error(t, err.e)
+		require.Error(t, err)
+		require.Error(t, err.e)
 		assert.Equal(t, underlyingErr, err.e)
 	})
 
 	t.Run("should create a new taskCanceledError with nil if the given error is nil", func(t *testing.T) {
 		err := newTaskCanceledError(nil)
-		assert.Error(t, err)
-		assert.Nil(t, err.e)
+		require.Error(t, err)
+		assert.NoError(t, err.e)
 	})
 }
 
@@ -30,15 +31,15 @@ func Test_taskCanceledError_Error(t *testing.T) {
 		const expectedMessage = "original error"
 		err := errors.New(expectedMessage)
 		taskErr := newTaskCanceledError(err)
-		assert.Error(t, taskErr)
+		require.Error(t, taskErr)
 		assert.Equal(t, taskErr.e, err)
 		assert.Equal(t, expectedMessage, taskErr.Error())
 	})
 
 	t.Run("should return an empty string if the underlying error is nil", func(t *testing.T) {
 		taskErr := newTaskCanceledError(nil)
-		assert.Error(t, taskErr)
-		assert.Nil(t, taskErr.e)
+		require.Error(t, taskErr)
+		require.NoError(t, taskErr.e)
 	})
 }
 
@@ -47,13 +48,13 @@ func Test_taskCanceledError_Unwrap(t *testing.T) {
 	t.Run("should return the underlying error", func(t *testing.T) {
 		underlyingErr := errors.New("original error")
 		taskErr := newTaskCanceledError(underlyingErr)
-		assert.Error(t, taskErr)
+		require.Error(t, taskErr)
 		assert.Equal(t, underlyingErr, taskErr.Unwrap())
 	})
 
 	t.Run("should return nil if the underlying error is nil", func(t *testing.T) {
 		taskErr := newTaskCanceledError(nil)
-		assert.Error(t, taskErr)
-		assert.Nil(t, taskErr.Unwrap())
+		require.Error(t, taskErr)
+		assert.NoError(t, taskErr.Unwrap())
 	})
 }
