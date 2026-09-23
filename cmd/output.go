f78a21af485999feafd701aa00938aa359178402
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-09-23 17:03:50 -0400

    refactor: renamed funcs, reorder funcs, added func comments

--- cmd/output.go
+++ cmd/output.go
@@ -14,82 +14,16 @@ import (
 )
 
 const (
-	lineNbrIndentBy = 2
-	lineSeparator   = "▐"
+	lineNbrIndentBy = 2   // Number of spaces to indent the line number column
+	lineSeparator   = "▐" // Separator between the line number and the content
+	padLinesBy      = 3   // Number of lines to pad before and after each uncovered line
 )
 
 var lineNbrIndent = strings.Repeat(" ", lineNbrIndentBy)
 
-func formatDimmedLine(maxLineDigits int, lineNbr int, lineContent string) string {
-	return pp.Dimf("%s%*d %s %s\n", lineNbrIndent, maxLineDigits, lineNbr, lineSeparator, lineContent)
-}
-
-func formatElipsisLine(maxLineDigits int) string {
-	return pp.Dimf("%s%*s %s\n", lineNbrIndent, maxLineDigits, "∙∙∙", lineSeparator)
-}
-
-func formatUncoveredLine(maxLineDigits int, lineNbr int, lineContent string) string {
-	boldLineNbr := pp.Boldf("%*d", maxLineDigits, lineNbr)
-
-	return fmt.Sprintf("%s%s%s\n", lineNbrIndent, boldLineNbr, pp.Redf(" %s %s", lineSeparator, lineContent))
-}
-
-// outputUncoveredLines writes the uncovered lines from the report to
-// [os.Stdout]. If an output file path is specified, the uncovered lines will
-// also be written to that file.
-func outputUncoveredLines(report *analyzer.Report, outputFilePath string) error {
-	if !report.HasUncoveredLines() {
-		return nil
-	}
-
-	var outputFile *os.File
-
-	if outputFilePath != "" {
-		var err error
-		outputFile, err = os.Create(outputFilePath)
-		if err != nil {
-			return fmt.Errorf("failed to create output file: %w", err)
-		}
-		defer outputFile.Close() //nolint:errcheck
-	}
-
-	fmt.Printf("%s\n\n", colors.LightGreen("Missing coverage:"))
-
-	for _, file := range report.Files {
-		if len(file.NewUncoveredNewLinesGroups) == 0 {
-			continue
-		}
-
-		// fmt.Println(pp.Bold(file.Path))
-
-		for _, funcName := range file.ASTFile.FuncOrder {
-			_, ok := file.NewUncoveredNewLinesGroups[funcName]
-			if !ok {
-				continue
-			}
-
-			outputUncoveredLineToStdout3(file, funcName)
-		}
-
-		// for funcName, lineRange := range file.NewUncoveredNewLinesGroups {
-		// 	_ = funcName
-		// 	_ = lineRange
-		// outputUncoveredLineToStdout(file.Path, lineRange)
-		// outputUncoveredLineToStdout3(file, funcName)
-		// outputUncoveredLinetoFile(outputFile, file.Path, lineRange)
-		// }
-
-		// if len(file.NewUncoveredNewLinesGroups) > 0 {
-		// 	fmt.Println()
-		// }
-	}
-
-	return nil
-}
-
-// outputUncoveredLineToStdout writes the uncovered line range for a given file
-// to the [os.Stdout].
-func outputUncoveredLineToStdout(filePath string, lineRange analyzer.LineRange) {
+// displayUncoveredLines displays the uncovered line range for a given file to
+// [os.Stdout].
+func displayUncoveredLines(filePath string, lineRange analyzer.LineRange) {
 	fmt.Printf("%s:%s:%s\n",
 		pp.Bold(filePath),
 		pp.Redf("%d", lineRange.Start),
@@ -97,9 +31,9 @@ func outputUncoveredLineToStdout(filePath string, lineRange analyzer.LineRange) 
 	)
 }
 
-// outputUncoveredLineToStdout writes the uncovered line range for a given file
-// to the [os.Stdout].
-func outputUncoveredLineToStdout3(file *analyzer.FileReport, funcName string) {
+// displayUncoveredFunctionLines displays the uncovered lines for a given
+// function within a file to [os.Stdout].
+func displayUncoveredFunctionLines(file *analyzer.FileReport, funcName string) {
 	f, ok := file.ASTFile.FuncDecls[funcName]
 	if !ok {
 		return
@@ -156,21 +90,97 @@ func outputUncoveredLineToStdout3(file *analyzer.FileReport, funcName string) {
 	fmt.Print(buf.String())
 }
 
-// outputUncoveredLinetoFile writes the uncovered line range for a given file to
+// formatDimmedLine formats a line with dimmed text for the line number and
+// content.
+func formatDimmedLine(maxLineDigits int, lineNbr int, lineContent string) string {
+	return pp.Dimf("%s%*d %s %s\n", lineNbrIndent, maxLineDigits, lineNbr, lineSeparator, lineContent)
+}
+
+// formatElipsisLine formats an ellipsis line with dimmed text for the line
+// number column.
+func formatElipsisLine(maxLineDigits int) string {
+	return pp.Dimf("%s%*s %s\n", lineNbrIndent, maxLineDigits, "∙∙∙", lineSeparator)
+}
+
+// formatUncoveredLine formats a line with the line number in bold and the
+// content in red to indicate it is uncovered.
+func formatUncoveredLine(maxLineDigits int, lineNbr int, lineContent string) string {
+	boldLineNbr := pp.Boldf("%*d", maxLineDigits, lineNbr)
+
+	return fmt.Sprintf("%s%s%s\n", lineNbrIndent, boldLineNbr, pp.Redf(" %s %s", lineSeparator, lineContent))
+}
+
+// outputUncoveredLines writes the uncovered lines from the report to
+// [os.Stdout]. If an output file path is specified, the uncovered lines will
+// also be written to that file.
+func outputUncoveredLines(report *analyzer.Report, outputFilePath string) error {
+	if !report.HasUncoveredLines() {
+		return nil
+	}
+
+	var outputFile *os.File
+
+	if outputFilePath != "" {
+		var err error
+		outputFile, err = os.Create(outputFilePath)
+		if err != nil {
+			return fmt.Errorf("failed to create output file: %w", err)
+		}
+		defer outputFile.Close() //nolint:errcheck
+	}
+
+	fmt.Printf("%s\n\n", colors.LightGreen("Missing coverage:"))
+
+	for _, file := range report.Files {
+		if len(file.NewUncoveredNewLinesGroups) == 0 {
+			continue
+		}
+
+		// fmt.Println(pp.Bold(file.Path))
+
+		for _, funcName := range file.ASTFile.FuncOrder {
+			lineRanges, ok := file.NewUncoveredNewLinesGroups[funcName]
+			if !ok {
+				continue
+			}
+
+			displayUncoveredFunctionLines(file, funcName)
+			outputUncoveredLinesToFile(outputFile, file.Path, lineRanges)
+		}
+	}
+
+	return nil
+}
+
+// outputUncoveredLineToFile writes the uncovered line range for a given file to
 // the specified output file.
-func outputUncoveredLinetoFile(file *os.File, filePath string, lineRange analyzer.LineRange) {
-	switch file {
-	case nil:
+func outputUncoveredLineToFile(file *os.File, filePath string, lineRange analyzer.LineRange) {
+	if file == nil {
 		return
-	default:
-		_, _ = fmt.Fprintf(file, "%s:%d:%d\n", filePath, lineRange.Start, lineRange.End)
+	}
+
+	_, _ = fmt.Fprintf(file, "%s:%d:%d\n", filePath, lineRange.Start, lineRange.End)
+}
+
+// outputUncoveredLinesToFile writes multiple uncovered line ranges for a given
+// file to the specified output file.
+func outputUncoveredLinesToFile(file *os.File, filepath string, lineRanges []analyzer.LineRange) {
+	if file == nil {
+		return
+	}
+
+	for _, lineRange := range lineRanges {
+		outputUncoveredLineToFile(file, filepath, lineRange)
 	}
 }
 
+// padLines takes a list of uncovered lines and pads them with additional lines
+// before and after each uncovered line within the function body range. It
+// returns a sorted and compacted list of line numbers.
 func padLines(lines []int, funcBodyLineStart, funcBodyLineEnd int) []int {
 	paddedUncoveredNewLines := make([]int, 0)
 	for _, line := range lines {
-		for i := line - 3; i <= line+3; i++ {
+		for i := line - padLinesBy; i <= line+padLinesBy; i++ {
 			if i >= funcBodyLineStart && i <= funcBodyLineEnd {
 				paddedUncoveredNewLines = append(paddedUncoveredNewLines, i)
 			}
