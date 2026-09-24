6e500eaf3f5d6ed69c8a86de1594a36e216763d2
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-09-23 20:45:45 -0400

    test: added test for displayUncoveredLines, added another assertion for displayUncoveredLine

--- cmd/output_test.go
+++ cmd/output_test.go
@@ -28,12 +28,35 @@ func Test_displayUncoveredLine(t *testing.T) {
 		contents, err := os.ReadFile(stdoutFile.Name())
 		require.NoError(t, err)
 		require.NotEmpty(t, contents)
+
+		contentsNoANSI := ansi.Strip(string(contents))
+
+		require.Contains(t, contentsNoANSI, "file.go:1:2")
+
 		t.Logf("Uncovered lines written to stdout:\n%s", string(contents))
 	})
 }
 
 // Tests for [displayUncoveredLines] function.
 func Test_displayUncoveredLines(t *testing.T) {
+	t.Run("should output uncovered lines to stdout", func(t *testing.T) {
+		stdoutFile := testutils.SetStdout(t)
+
+		displayUncoveredLines("file.go", []analyzer.LineRange{
+			{Start: 1, End: 2},
+			{Start: 3, End: 4},
+		})
+
+		contents, err := os.ReadFile(stdoutFile.Name())
+		require.NoError(t, err)
+		require.NotEmpty(t, contents)
+
+		contentsNoANSI := ansi.Strip(string(contents))
+
+		require.Contains(t, contentsNoANSI, "file.go:1:2")
+		require.Contains(t, contentsNoANSI, "file.go:3:4")
+		t.Logf("Uncovered lines written to stdout:\n%s", string(contents))
+	})
 }
 
 // Tests for [displayUncoveredFunctionLines] function.
